cd4b93e397fde1bac0331a0dab6046be691cc999
echo <echo01248@gmail.com>
2017-09-18 22:38:19 -0700

    source for zsh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,15 @@ fi
 
 EOL
 
+grep "# Source dotfiles/shrc" ~/.zshrc 2>/dev/null 1>/dev/null || \
+cat >>~/.zshrc <<EOL
+
+# Source dotfiles/shrc
+if [ -f ~/dotfiles/shrc ]; then
+    source ~/dotfiles/shrc
+fi
+
+EOL
 
 ln -s -f ~/dotfiles/vimrc ~/.vimrc
 ln -s -f ~/dotfiles/tmux.conf ~/.tmux.conf
