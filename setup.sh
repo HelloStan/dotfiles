5c3a01fc85d7433a83b4bdddcaf100de2c987180
echo <echo01248@gmail.com>
2017-11-15 23:03:35 -0800

    install with custom repo name

--- setup.sh
+++ setup.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+dotfiles_path=`pwd`
 
-grep "# Source dotfiles/shrc" ~/.bashrc 2>/dev/null 1>/dev/null || \
+grep "# Source $dotfiles_path/shrc" ~/.bashrc 2>/dev/null 1>/dev/null || \
 cat >>~/.bashrc <<EOL
 
 # Source dotfiles/shrc
-if [ -f ~/dotfiles/shrc ]; then
-    source ~/dotfiles/shrc
+if [ -f $dotfiles_path/shrc ]; then
+    source $dotfiles_path/shrc
 fi
 
 EOL
 
-grep "# Source dotfiles/shrc" ~/.zshrc 2>/dev/null 1>/dev/null || \
+grep "# Source $dotfiles_path/shrc" ~/.zshrc 2>/dev/null 1>/dev/null || \
 cat >>~/.zshrc <<EOL
 
 # Source dotfiles/shrc
-if [ -f ~/dotfiles/shrc ]; then
-    source ~/dotfiles/shrc
+if [ -f $dotfiles_path/shrc ]; then
+    source $dotfiles_path/shrc
 fi
 
 EOL
 
-ln -s -f ~/dotfiles/vimrc ~/.vimrc
-ln -s -f ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s -f $dotfiles_path/vimrc ~/.vimrc
+ln -s -f $dotfiles_path/tmux.conf ~/.tmux.conf
 
