943653b346872772396135a7043f71c439f30af9
echo <echo01248@gmail.com>
2018-01-21 22:20:49 -0800

    [setup] .ideavim and prompt to set .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dotfiles_path=`pwd`
 
+echo "Source shrc in .bashrc"
 grep "# Source $dotfiles_path/shrc" ~/.bashrc 2>/dev/null 1>/dev/null || \
 cat >>~/.bashrc <<EOL
 
@@ -11,6 +12,7 @@ fi
 
 EOL
 
+echo "Source shrc in .zshrc"
 grep "# Source $dotfiles_path/shrc" ~/.zshrc 2>/dev/null 1>/dev/null || \
 cat >>~/.zshrc <<EOL
 
@@ -21,7 +23,11 @@ fi
 
 EOL
 
+echo "Set symlinks for .vimrc .ideavim .tmux.confy"
 ln -s -f $dotfiles_path/vimrc ~/.vimrc
+ln -s -f $dotfiles_path/vimrc ~/.ideavimrc
 ln -s -f $dotfiles_path/tmux.conf ~/.tmux.conf
-ln -s -f $dotfiles_path/gitconfig ~/.gitconfig
+
+read -e -p "Set .gitconfig? " choice
+[[ "$choice" == [Yy]* ]] && ln -s -f $dotfiles_path/gitconfig ~/.gitconfig
 
